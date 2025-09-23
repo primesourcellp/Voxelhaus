aafef97781bad6d2eeea0ae0a027443c1b19a587
primesourcellp <recruit@primesourcellp.com>
2025-09-23 17:01:40 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/Voxelhaus">
       <div className="min-h-screen bg-slate-900 text-white font-sans">
         <Navbar />
         <main>
